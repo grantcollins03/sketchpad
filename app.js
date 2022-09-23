01b582520c382a9ff904c174b0e3b0ea6722b17e
Grant Collins <grantcollins03@gmail.com>
2022-09-23 17:51:51 -0400

    Created 16x16 grid

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let container = document.getElementById("container");
 
-function addGrid(size) {
-    for (i = 0; i < (size ** 2); i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        container.appendChild(square);
+function addGrid(height, width) {
+    for (i = 0; i < height; i++) {
+        let row = document.createElement("div");
+        container.appendChild(row);
+        for (j = 0; j < width; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
     }
 }
 
-addGrid(16);
+addGrid(16, 16);
