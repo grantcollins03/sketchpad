d22a95ec06885951874bcaab82a6e77456415405
Grant Collins <grantcollins03@gmail.com>
2022-09-23 17:22:57 -0400

    Added addGrid()

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let container = document.getElementById("container");
 
-let square = document.createElement("div");
-square.innerText = "Hi";
+function addGrid(size) {
+    for (i = 0; i < (size ** 2); i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+}
 
-container.appendChild(square);
+addGrid(16);
