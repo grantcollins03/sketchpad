4476990af2559b0f873cfe8318ba61b387138a51
Grant Collins <grantcollins03@gmail.com>
2022-09-24 19:34:30 -0400

    Tried changing color of squares on hover w/o success

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let container = document.getElementById("container");
 
 function addGrid(height, width) {
-    for (i = 0; i < height; i++) {
-        let row = document.createElement("div");
-        container.appendChild(row);
-        for (j = 0; j < width; j++) {
-            let square = document.createElement("div");
-            square.classList.add("square");
-            row.appendChild(square);
-        }
+  for (i = 0; i < height; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (j = 0; j < width; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
     }
+  }
 }
 
+
 addGrid(16, 16);
+
+/*
+
+let row = container.querySelector("row");
+let square = row.querySelector("square");
+
+square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "blue";
+});
+
+*/
