6813ba405a2da8997a0b44716496110edca3fc1e
Grant Collins <grantcollins03@gmail.com>
2022-09-27 17:51:37 -0400

    Created evenly sized grid; added hover effect

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-let gridContainer = document.getElementById("grid-container");
+let grid = document.getElementById("grid");
 
 function createGrid(num) {
   for (i = 0; i < num ** 2; i++) {
     let cell = document.createElement("div");
     cell.className = "cell";
-    gridContainer.appendChild(cell);
-    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    cell.id = "cell";
+    grid.appendChild(cell);
+    grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    grid.style.width = `${num}px`
   }
 }
 
 createGrid(16);
+
+let cell = document.getElementById("cell");
+
+document.body.addEventListener("mouseover", function mouseOver(event) {
+  if (event.target.className.toLowerCase() === "cell") {
+    event.target.style.backgroundColor = "black";
+  }
+});
